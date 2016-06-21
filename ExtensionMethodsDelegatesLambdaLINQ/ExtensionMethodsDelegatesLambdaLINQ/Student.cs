ccbd2a4871ee5347d48c51817d97ba89636da449
Eli Velichkova <calipsomn@yahoo.com>
2016-06-21 15:08:59 +0300

    Homework

---
+++ ExtensionMethodsDelegatesLambdaLINQ/ExtensionMethodsDelegatesLambdaLINQ/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsDelegatesLambdaLINQ
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
