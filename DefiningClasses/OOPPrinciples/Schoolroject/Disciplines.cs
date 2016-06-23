951278c83a112eec38c99210b5bb9f7cb9072260
calipsomn <calipsomn@yahoo.com>
2016-06-24 00:32:43 +0300

    homework

---
+++ DefiningClasses/OOPPrinciples/Schoolroject/Disciplines.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolroject
+{
+    public class Disciplines
+    {
+        private string name;
+        private int numOfExercices;
+        private int numOfLectures;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int NumOfExercices
+        {
+            get { return numOfExercices; }
+            set { numOfExercices = value; }
+        }
+
+        public int NumOfLectures
+        {
+            get { return numOfLectures; }
+            set { numOfLectures = value; }
+        }
+
+        public Disciplines(string name, int numOfExercices, int numOfLectures)
+        {
+            this.name = name;
+            this.numOfExercices = numOfExercices;
+            this.numOfLectures = numOfLectures;
+        }
+    }
+}
---
+++ DefiningClasses/OOPPrinciples/Schoolroject/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolroject
+{
+    public class Student:Human
+    {
+        private int classNumber;
+
+        public int ClassNumber
+        {
+            get { return classNumber; }
+            set { classNumber = value; }
+        }
+
+        public Student(string name, int classNumber):base(name)
+        {
+            this.classNumber = classNumber;
+        }
+    }
+}
