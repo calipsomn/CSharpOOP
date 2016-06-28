738980125d8638688b12bf0a7b7161932f7f8276
calipsomn <calipsomn@yahoo.com>
2016-06-28 22:01:06 +0300

    homework

---
+++ DefiningClasses/OOPPrinciples/Schoolroject/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolroject
+{
+    public abstract class Human
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Human(string name)
+        {
+            this.name = name;
+        }
+    }
+}
