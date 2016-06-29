a252d02d0a5985e030d7ca99676602ad09bc849d
Eli Velichkova <calipsomn@yahoo.com>
2016-06-29 18:03:22 +0300

    Changes

---
+++ DefiningClassesPart2/DefiningClassesPart2/Version.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClassesPart2
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Method | System.AttributeTargets.Interface |
+                       System.AttributeTargets.Enum | System.AttributeTargets.Struct)]
+    public class Version:System.Attribute
+    {
+        private int major;
+        private int minor;
+
+        public int Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+
+        public int Minor
+        {
+            get { return minor; }
+            set { minor = value; }
+        }
+
+        public Version(int major, int minor)
+        {
+            this.major = major;
+            this.minor = minor;
+        }
+    }
+}
